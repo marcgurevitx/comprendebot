b12b6e4e4dcb129091d922a75633c244c4f003f2
Marc Gurevitx <marcgurevitx@gmail.com>
2021-01-31 06:14:56 +0300

    feature-chooser: Chooser server

--- code/chooser/__main__.py
+++ code/chooser/__main__.py
@@ -1,2 +1,13 @@
+from goodenough import GoodEnough
 
-print("this is choooooooser")
+from botcommon.config import config
+
+
+async def get_items(request):
+    return ["YOUR_CHALLENGE_7"]
+
+
+chooser = GoodEnough(
+    get_items=get_items,
+)
+chooser.serve(port=config.CHOOSER_PORT)
