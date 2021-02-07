6f8645f8b1fc6cf6b78e583bbac5c9338345d405
Marc Gurevitx <marcgurevitx@gmail.com>
2021-02-07 11:01:45 +0300

    feature-vocs: Show phrases choice

--- code/bot/handlers.py
+++ code/bot/handlers.py
@@ -32,11 +32,14 @@ async def on_edit(message, *, person, chat, **kwargs):
 async def on_button_press(callback_query, *, person, chat, **kwargs):
     challenge = await person.get_existing_active_challenge()
     if challenge:
+        await callback_query.answer("[TTT] Processing...")
         async with challenge.get_executor() as executor:
             await executor.receive_button_press(callback_query.data)
             sendables = executor.pop_sendables()
             await chat.send_list(sendables)
+        callback_answer = ""
     elif callback_query.data == config.CMPDBOT_CONST_START:
+        await callback_query.answer("[TTT] Starting...")
         await arrange_new_challenge(person, chat)
     else:
         await callback_query.answer("[TTT] Stale button")
--- code/botcommon/executors/voice.py
+++ code/botcommon/executors/voice.py
@@ -1,6 +1,6 @@
-from botcommon.bottypes import VoiceStates
+from psycopg2.extras import Json
 
-from botcommon.bottypes import Sendable, Button, SendableTypeCode
+from botcommon.bottypes import VoiceStates, Sendable, Button, SendableTypeCode
 
 from .baseexecutor import BaseExecutor
 
@@ -15,12 +15,31 @@ class VoiceExecutor(BaseExecutor):
             dest=VoiceStates.VOC_WRK,
             after="explain_challenge",
         ),
+        dict(
+            trigger="receive_button_press",
+            source=VoiceStates.VOC_WRK,
+            dest=VoiceStates.VOC_WRK,
+            conditions=["is_phrase_pressed"],
+            after="ask_voice",
+        ),
+        
+        
+        # ...
+        
+        
+        #dict(
+        #    trigger="receive_button_press",
+        #    source=PhraseStates.VOC_WRK,
+        #    dest=PhraseStates.VOC_END,
+        #    conditions=["is_submit_pressed"],
+        #    after="save_voice",
+        #),
     ]
 
     async def explain_challenge(self):
         phrases = await self.challenge.get_phrases()
         buttons = [
-            Button(text=p.row.original_text, data=p.row.id)
+            Button(text=p.row.original_text, data=f"p:{p.row.id}")
             for p
             in phrases
         ]
@@ -29,5 +48,23 @@ class VoiceExecutor(BaseExecutor):
             value="[TTT] Choose phrase.",
             is_reply=False,
             buttons=buttons,
+        )
+        self.sendables.append(s)
+
+    async def is_phrase_pressed(self, callback_data):
+        return callback_data.startswith("p:")
+
+    async def ask_voice(self, callback_data):
+        phrase_id = int(callback_data[2:])
+
+        executor_data = self.challenge.row.executor_data
+        executor_data["phrase_id"] = phrase_id
+        await self.challenge.update(executor_data=Json(executor_data))
+
+        s = Sendable(
+            type=SendableTypeCode.SND_TXT,
+            value="[TTT] Now send voice recording.",
+            is_reply=False,
+            buttons=[],
         )
         self.sendables.append(s)
